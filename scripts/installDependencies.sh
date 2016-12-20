de3398f107056ec667bea86be9998c312538d14c
tarTG <tgerdi@web.de>
2016-12-20 10:56:23 +0100

    fix in install script

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -2,7 +2,7 @@
 set -ex
 
 
-add-apt-repository -y ppa:pyglfw/pyglfw && sudo apt-get update -qq && sudo apt-get install -y --no-install-recommends g++ libglut1-mesa-dev freeglut3-dev mesa-common-dev libglfw3-dev libxrandr-dev libxi-dev libxxf86vm-dev libxcursor1 libxcursor-dev libxinerama-dev libnoise-dev libglm-dev libdevil-dev 
+add-apt-repository -y ppa:pyglfw/pyglfw && sudo apt-get update -qq && sudo apt-get install -y --no-install-recommends g++ libglu1-mesa-dev freeglut3-dev mesa-common-dev libglfw3-dev libxrandr-dev libxi-dev libxxf86vm-dev libxcursor1 libxcursor-dev libxinerama-dev libnoise-dev libglm-dev libdevil-dev 
 
 #install anttweak bar
 mkdir anttweakbar
