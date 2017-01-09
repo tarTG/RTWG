bd1fd511b918ed01a11a7a18f915694445f5d76a
TG <tgerdi@web.de>
2017-01-09 11:33:58 +0100

    fix for older Ubunut versions

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
+if [[ 'lsb_release -ds' != "Ubuntu 16.04" ]] # glfw allready in the ubuntu 16.04 repository
+then
+    add-apt-repository -y ppa:pyglfw/pyglfw && sudo apt-get update -qq
+fi
 
 apt-get install -y --no-install-recommends g++ libglu1-mesa-dev freeglut3-dev mesa-common-dev libglfw3-dev libxrandr-dev libxi-dev libxxf86vm-dev libxcursor1 libxcursor-dev libxinerama-dev libnoise-dev libglm-dev libdevil-dev 
 
